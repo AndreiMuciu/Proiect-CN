cfa5f31a90a13ebd5b10fe6ebe2bbbbe39d0aff2
Edward326 <eduardvesea@gmail.com>
2024-05-19 14:52:14 +0300

    fctOFDetectionBlockInCacheDONE

--- Cache-Partea2/centralUnit.v
+++ Cache-Partea2/centralUnit.v
@@ -7,16 +7,18 @@ output reg [6:0] cSig
 );
 localparam s0 = 1;
 localparam s1 = 2;
-localparam s2 = 4;
-localparam s3 = 8;
-localparam s4 = 16;
-localparam s5 = 32;
-localparam s6 = 64;
-localparam s7 = 128;
-localparam s8 = 256;
-localparam s9 = 512;
+localparam sREADINT=4;
+localparam s2 = 8;
+localparam s3 = 16;
+localparam s4 = 32;
+localparam s5 = 64;
+localparam s6 = 128;
+localparam s7 = 256;
+localparam sWRITEINT=512;
+localparam s8 = 1024;
+localparam s9 = 2048;
 
-reg [9:0] cst,nxst;
+reg [11:0] cst,nxst;
 
 always @* begin
 case(cst)
@@ -31,6 +33,10 @@ nxst=s0;
 end
 
 s1:begin
+nxst=sREADINT;
+end
+
+sREADINT:begin
 if(CacheHIT==1)
 nxst=s2;
 else
@@ -64,6 +70,10 @@ nxst=s1;
 end
 
 s7:begin
+nxst=sWRITEINT;
+end
+  
+sWRITEINT:begin
 if(CacheHIT==1)
 nxst=s9;
 else
@@ -93,7 +103,7 @@ always @* begin
         s6:cSig=7'd32;
         s7:cSig=7'd2;
         s9:cSig=7'd64;
-        
+      
         default: cSig=7'd0;
     endcase
 end
@@ -122,19 +132,22 @@ endmodule
 module selectWord(
 input [6:0] tag,
 input [6:0] index,
-input [523:0] cacheARRAY [127:0][3:0],
-output reg [3:0] set
+output reg [3:0] set,
+input [523:0] cacheBANK_A [127:0],
+input [523:0] cacheBANK_B [127:0],
+input [523:0] cacheBANK_C [127:0],
+input [523:0] cacheBANK_D [127:0]
 );
 wire eq1,eq2,eq3,eq4;
-comparator comp1( .a(cacheARRAY[index][0][11:5]), .b(tag), .eq(eq1));
-comparator comp2( .a(cacheARRAY[index][1][11:5]), .b(tag), .eq(eq2));
-comparator comp3( .a(cacheARRAY[index][2][11:5]), .b(tag), .eq(eq3));
-comparator comp4( .a(cacheARRAY[index][3][11:5]), .b(tag), .eq(eq4));
+comparator comp1( .a(cacheBANK_A[index][11:5]), .b(tag), .eq(eq1));
+comparator comp2( .a(cacheBANK_B[index][11:5]), .b(tag), .eq(eq2));
+comparator comp3( .a(cacheBANK_C[index][11:5]), .b(tag), .eq(eq3));
+comparator comp4( .a(cacheBANK_D[index][11:5]), .b(tag), .eq(eq4));
 always @* begin
-set[0] <=eq1&cacheARRAY[index][0][1];
-set[1] <=eq2&cacheARRAY[index][1][1];
-set[2] <=eq3&cacheARRAY[index][2][1];
-set[3] <=eq4&cacheARRAY[index][3][1];
+set[0] <=eq1&cacheBANK_A[index][1];
+set[1] <=eq2&cacheBANK_B[index][1];
+set[2] <=eq3&cacheBANK_C[index][1];
+set[3] <=eq4&cacheBANK_D[index][1];
 end
 endmodule
 
@@ -281,10 +294,10 @@ input s0,s1,s2,s3,
 output reg [3:0] eqNFULL
 );
 always @* begin
-eqNFULL[0]<=eqNFULL[0]&eqNFULL[1]&eqNFULL[2]&eqNFULL[3];
-eqNFULL[1]<=(~eqNFULL[0])&eqNFULL[1]&eqNFULL[2]&eqNFULL[3];
-eqNFULL[2]<=(~eqNFULL[0])&(~eqNFULL[1])&eqNFULL[2]&eqNFULL[3];
-eqNFULL[3]<=(~eqNFULL[0])&(~eqNFULL[1])&(~eqNFULL[2])&eqNFULL[3];
+  eqNFULL[0]<=s0&s1&s2&s3;
+  eqNFULL[1]<=(~s0)&s1&s2&s3;
+  eqNFULL[2]<=(~s0)&(~s1)&s2&s3;
+  eqNFULL[3]<=(~s0)&(~s1)&(~s2)&s3;
 end
 endmodule
 
@@ -292,7 +305,10 @@ endmodule
 
 module detectBlockInCache(
 input clk,
-input [523:0] cacheARRAY [127:0][3:0],
+input [523:0] cacheBANK_A [127:0],
+input [523:0] cacheBANK_B [127:0],
+input [523:0] cacheBANK_C [127:0],
+input [523:0] cacheBANK_D [127:0],
 input [19:0] adressWord,
 input signal,
 input CacheHIT_IN,CacheFULL_IN,
@@ -315,10 +331,11 @@ wire [3:0] eqFULL,eqNFULL;
 wire [1:0] setFULL,setNFULL;
 wire [1:0] setHIT,setNHIT;
 
-selectWord inst(.tag(adressWord[19:13]), .index(adressWord[12:6]), .cacheARRAY(cacheARRAY), .set(set));
+selectWord inst(.tag(adressWord[19:13]), .index(adressWord[12:6]), .set(set),
+                .cacheBANK_A(cacheBANK_A), .cacheBANK_B(cacheBANK_B), .cacheBANK_C(cacheBANK_C), .cacheBANK_D(cacheBANK_D));
 
-mux4to1 blockSET(.data_in0(cacheARRAY[adressWord[12:6]][0][523:12]), .data_in1(cacheARRAY[adressWord[12:6]][1][523:12]),
-                 .data_in2(cacheARRAY[adressWord[12:6]][2][523:12]), .data_in3(cacheARRAY[adressWord[12:6]][3][523:12]), 
+mux4to1 blockSET(.data_in0(cacheBANK_A[adressWord[12:6]][523:12]), .data_in1(cacheBANK_B[adressWord[12:6]][523:12]),
+                 .data_in2(cacheBANK_C[adressWord[12:6]][523:12]), .data_in3(cacheBANK_D[adressWord[12:6]][523:12]), 
                  .select(set[3:0]), .data_out(selblk));
 mux8to1A wordSET(.data_in0(selblk[63:0]), .data_in1(selblk[127:64]), .data_in2(selblk[191:128]),
                  .data_in3(selblk[255:192]), .data_in4(selblk[319:256]), .data_in5(selblk[383:320]),
@@ -331,14 +348,14 @@ mux8to1B byteSET(.data_in0(selword[7:0]), .data_in1(selword[15:8]), .data_in2(se
 mux2to1 byteSETFINAL(.data_in0(wordSEL_IN), .data_in1(selbyte), .select(signal), .data_out(wordSEL));
 //pt wordSEL
 
-assign CacheFULL=~(cacheARRAY[adressWord[12:6]][0][0] | cacheARRAY[adressWord[12:6]][1][0] | cacheARRAY[adressWord[12:6]][2][0] | cacheARRAY[adressWord[12:6]][3][0]);
+assign CacheFULL=~(cacheBANK_A[adressWord[12:6]][0] | cacheBANK_B[adressWord[12:6]][0] | cacheBANK_C[adressWord[12:6]][0] | cacheBANK_D[adressWord[12:6]][0]);
 assign CacheHIT=(set[0] | set[1] | set[2] | set[3]);
 //pt CacheHIT CacheFULL
 
-eqFULLM eqinst1(.age0(cacheARRAY[adressWord[12:6]][0][4:3]), .age1(cacheARRAY[adressWord[12:6]][1][4:3]),
-                .age2(cacheARRAY[adressWord[12:6]][2][4:3]), .age3(cacheARRAY[adressWord[12:6]][3][4:3]), .eqFULL(eqFULL));
-eqNFULLM eqinst2(.s0(cacheARRAY[adressWord[12:6]][0][0]), .s1(cacheARRAY[adressWord[12:6]][1][0]),
-                .s2(cacheARRAY[adressWord[12:6]][2][0]), .s3(cacheARRAY[adressWord[12:6]][3][0]), .eqNFULL(eqNFULL));
+eqFULLM eqinst1(.age0(cacheBANK_A[adressWord[12:6]][4:3]), .age1(cacheBANK_B[adressWord[12:6]][4:3]),
+                .age2(cacheBANK_C[adressWord[12:6]][4:3]), .age3(cacheBANK_D[adressWord[12:6]][4:3]), .eqFULL(eqFULL));
+eqNFULLM eqinst2(.s0(cacheBANK_A[adressWord[12:6]][0]), .s1(cacheBANK_B[adressWord[12:6]][0]),
+                .s2(cacheBANK_C[adressWord[12:6]][0]), .s3(cacheBANK_D[adressWord[12:6]][0]), .eqNFULL(eqNFULL));
                 
 mux4to1FULL fullMUX(.data_in0(2'd0), .data_in1(2'd1), .data_in2(2'd2), .data_in3(2'd3), .select(eqFULL[3:0]), .data_out(setFULL));
 mux4to1FULL nfullMUX(.data_in0(2'd0), .data_in1(2'd1), .data_in2(2'd2), .data_in3(2'd3), .select(eqNFULL[3:0]), .data_out(setNFULL));
@@ -367,7 +384,12 @@ input EN,W_R,
 output reg [7:0] wordSELOUT
 );
 
-reg [523:0] cacheARRAY [127:0][3:0];//128 lines with 3 columns on each element being stored 524b(all fields from one bank)
+//4 matrices of dimension:128 lines with 524 columns(all fields from one banks's cache index/line/no_set)
+reg [523:0] cacheBANK_A [127:0];
+reg [523:0] cacheBANK_B [127:0];
+reg [523:0] cacheBANK_C [127:0];
+reg [523:0] cacheBANK_D [127:0];
+
 reg CacheHIT,CacheFULL;
 reg [1:0] SET;
 reg [7:0] wordSEL;
@@ -379,27 +401,33 @@ wire [7:0] wordSELaux;
 reg rst=0,sec=0;
 
 integer i, j;
-always @(posedge clk) begin
+  always @(posedge clk) begin
     if(!rst) begin
      wordSEL<=8'd0;CacheHIT<=0;CacheFULL<=0;SET<=2'd0;
   for (i = 0; i < 128; i = i + 1) begin
-    for (j = 0; j < 4; j = j + 1) begin
-      cacheARRAY[i][j]<=524'd0;
-      cacheARRAY[i][j][0]<=1'b1;
-    end
+      cacheBANK_A[i]<=524'd0;cacheBANK_B[i]<=524'd0;cacheBANK_C[i]<=524'd0;cacheBANK_D[i]<=524'd0;
+      cacheBANK_A[i][0]<=1'b1;cacheBANK_B[i][0]<=1'b1;cacheBANK_C[i][0]<=1'b1;cacheBANK_D[i][0]<=1'b1;
   end
+      /*
+      //cache test for line1 word8 wordOffB=0
+      //cacheBANK_D[1][1]<=1'b1;cacheBANK_D[1][523:460]<=64'hFFFFFFFFFFFFFFFF;
+      //cacheBANK_A[1][0]<=1'b0;cacheBANK_B[1][0]<=1'b0;cacheBANK_C[1][0]<=1'b0;cacheBANK_D[1][0]<=1'b0;
+      //cacheBANK_D[1][4:3]<=3;
+      */ 
     end
 end
 
 controlUnit fsm(.clk(clk), .rst_b(rst), .EN(EN), .W_R(W_R), .CacheHIT(CacheHIT), .CacheFULL(CacheFULL), .cSig(cSig));
-detectBlockInCache detect(.clk(clk), .cacheARRAY(cacheARRAY), .adressWord(adressWord), .signal(cSig[1]),
-                   .CacheHIT_IN(CacheHIT), .wordSEL_IN(wordSEL), .CacheFULL_IN(CacheFULL), .SET_IN(SET),
-                   .CacheHIT_OUT(CacheHITaux), .wordSEL_OUT(wordSELaux), .CacheFULL_OUT(CacheFULLaux), .SET_OUT(SETaux));
-//other functions,to be wr
+detectBlockInCache detect(.clk(clk), .adressWord(adressWord), .signal(cSig[1]),
+                          .cacheBANK_A(cacheBANK_A), .cacheBANK_B(cacheBANK_B), .cacheBANK_C(cacheBANK_C), .cacheBANK_D(cacheBANK_D),
+                          .CacheHIT_IN(CacheHIT), .wordSEL_IN(wordSEL), .CacheFULL_IN(CacheFULL), .SET_IN(SET),
+                          .CacheHIT_OUT(CacheHITaux), .wordSEL_OUT(wordSELaux), .CacheFULL_OUT(CacheFULLaux), .SET_OUT(SETaux));
+//...other functions,to be written
 
 always @(posedge clk) begin
-    if(cSig[6] | cSig[2] | cSig[0])begin 
-    wordSEL<=8'd0; CacheHIT<=0;CacheFULL<=0;SET<=2'd0;//nu reinit cacheARRAY
+  $monitor("rst=%b  cSig=%b  HIT=%b  wordSEL=%d  SET=%b  FULL=%b",rst,cSig,CacheHIT,wordSELOUT,SET,CacheFULL);
+    if(cSig[6] | cSig[2] | cSig[0])begin
+    wordSEL<=8'd0; CacheHIT<=0;CacheFULL<=0;SET<=2'd0;//nu reinit niciun bank de cache
     end
     else begin
     CacheHIT<=CacheHITaux;
@@ -417,3 +445,45 @@ endmodule
 
 
 
+
+
+
+
+
+
+module centralUnit_tb;
+  reg [19:0] adressWord;
+  reg [63:0] data_in;
+  reg clk;
+  reg EN,W_R;
+  wire [7:0] wordSELOUT;
+  
+  centralUnit test(.adressWord(adressWord), .data_in(data_in),
+                   .clk(clk), .EN(EN), .W_R(W_R), .wordSELOUT(wordSELOUT));
+  initial begin
+    //$monitor("%b\n",wordSELOUT);
+    //minclocksRST_EN=2cc
+    //avgclocksADDRESSHIT=5cc
+    //avgclocksADRESSMISS=10cc
+    //1cc=2*10ns
+    
+    //1request
+    adressWord=20'b00000000000001111000;//linia/index=1 word=8 wordOffB=0
+    EN=1'b1;
+    W_R=1'b0;
+    #50;//wait minclocksRST_EN cycles from start,for fsm to exit s0 state(independent on EN)     =2cc x 20ns +10buff
+    EN=1'b0;//then we rest the en until the next request
+    #210;//the cache will be quicker thatn this waitTime,causing it to wait for a next request  =(miss)10cc x 20ns +10 buff
+    
+    //another requests.....
+    
+    $finish;
+  end
+  
+  localparam run_cycle=10,cycles=50;
+initial begin
+  clk=1'b0;
+  repeat (cycles*2)
+#run_cycle clk=~clk;
+end
+endmodule
